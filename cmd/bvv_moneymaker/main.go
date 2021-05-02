ede73d3568807837c2d06800096f2d42a0733976
Sebastiaan Mannem <sebastiaan.mannem@enterprisedb.com>
2021-05-02 12:00:34 +0200

    Small cleanup in cmd/bvv_moneymaker/main.go

--- cmd/bvv_moneymaker/main.go
+++ cmd/bvv_moneymaker/main.go
@@ -1,25 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 
 	"github.com/sebasmannem/bvvmoneymaker/internal"
 )
-
-// Use this definition to make passing optionals easier.
-// e.g. bitvavo.Markets(Options{ "market": "BTC-EUR" })
-type Options map[string]string
-
-// Use this function to print a human readable version of the returned struct.
-func PrettyPrint(v interface{}) (err error) {
-	b, err := json.MarshalIndent(v, "", "  ")
-	if err == nil {
-		fmt.Println(string(b))
-	}
-	return
-}
 
 func main() {
 	bvv, err := internal.NewBvvHandler()
